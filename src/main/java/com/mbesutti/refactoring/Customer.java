f4ccaa693569be2285a7ff974b03248628ba7ad6
Matteo Besutti <matteo.besutti@gmtech.ch>
2016-09-07 17:15:29 +0200

    all refactorings of 1st chapter Fowler done

--- src/main/java/com/mbesutti/refactoring/Customer.java
+++ src/main/java/com/mbesutti/refactoring/Customer.java
@@ -5,7 +5,7 @@ import java.util.Vector;
 
 public class Customer {
 	private final String _name;
-	private final Vector _rentals = new Vector();
+	private final Vector<Rental> _rentals = new Vector<Rental>();
 
 	public Customer(String name) {
 		_name = name;
@@ -20,27 +20,53 @@ public class Customer {
 	};
 
 	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		Enumeration rentals = _rentals.elements();
+		Enumeration<Rental> rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasMoreElements()) {
-			Rental each = (Rental) rentals.nextElement();
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
-				frequentRenterPoints++;
-			// show figures for this rental
+			Rental each = rentals.nextElement();
+			
 			result += "\t" + each.getMovie().getTitle() + "\t"
 					+ String.valueOf(each.getCharge()) + "\n";
-			totalAmount += each.getCharge();
 		}
-		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints())
 				+ " frequent renter points";
+		return result;
+	}
+
+	public String htmlStatement() {
+		Enumeration<Rental> rentals = _rentals.elements();
+		String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+		while (rentals.hasMoreElements()) {
+			Rental each = rentals.nextElement();
+			result += each.getMovie().getTitle() + ": "
+					+ String.valueOf(each.getCharge()) + "<BR>\n";
+		}
+		result += "<P>You owe <EM>" + String.valueOf(getTotalCharge())
+				+ "</EM><P>\n";
+		result += "On this rental you earned <EM>"
+				+ String.valueOf(getTotalFrequentRenterPoints())
+				+ "</EM> frequent renter points<P>";
+		return result;
+	}
+
+	private double getTotalCharge() {
+		double result = 0;
+		Enumeration<Rental> rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = rentals.nextElement();
+			result += each.getCharge();
+		}
+		return result;
+	}
+
+	private int getTotalFrequentRenterPoints() {
+		int result = 0;
+		Enumeration<Rental> rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = rentals.nextElement();
+			result += each.getFrequentRenterPoints();
+		}
 		return result;
 	}
 }
--- src/main/java/com/mbesutti/refactoring/Movie.java
+++ src/main/java/com/mbesutti/refactoring/Movie.java
@@ -4,23 +4,31 @@ public class Movie {
 	public static final int CHILDRENS = 2;
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
-	private String _title;
-	private int _priceCode;
+	private final String _title;
+	private Price _priceCode;
 
-	public Movie(String title, int priceCode) {
+	public Movie(String title, Price priceCode) {
 		_title = title;
 		_priceCode = priceCode;
 	}
 
-	public int getPriceCode() {
+	public Price getPriceCode() {
 		return _priceCode;
 	}
 
-	public void setPriceCode(int arg) {
+	public void setPriceCode(Price arg) {
 		_priceCode = arg;
 	}
 
 	public String getTitle() {
 		return _title;
+	}
+
+	double getCharge(int daysRented) {
+		return getPriceCode().getCharge(daysRented);
+	}
+
+	public int getFrequentRenterPoints(int daysRented) {
+		return _priceCode.getFrequentRenterPoints(daysRented);
 	};
 }
